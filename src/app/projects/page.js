8e96b9366f96181e61717a4363c1c5b87066c3a5
lakshankd <dominiclakshank@gmail.com>
2023-06-22 23:04:54 +0530

    Adding contants folder to hold data.

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -4,23 +4,10 @@ import styles from "./page.module.css";
 import ProjectCard from "@/components/project-card/ProjectCard";
 import Footer from "@/components/footer/Footer";
 import Button from "@/components/button/Button";
+import { projectsData } from "@/constants/index";
 
 export default function Projetcs() {
-  const projects = [
-    {
-      id: 1,
-      date: "June 14, 2023",
-      name: "Websafe",
-      desc: "The Cookie Consent Management System is an open-source tool that helps websites comply with laws and regulations related to cookies and tracking technologies. It provides website visitors with information about the types of cookies being used, while also giving them the option to control their cookie preferences.",
-      link: "https://github.com/Team-Infolinx",
-    },
-    {
-      id: 2,
-      date: "May 15, 2022",
-      name: "Automated Cake Slicing Machine",
-      desc: "A hardware project utilizing an Atmega32 Microcontroller to create an autonomous cake slicing machine. Users can slice cakes based on entered weight, ensuring precise and consistent portions.",
-    },
-  ];
+  const projects = projectsData;
   return (
     <div className={styles.container}>
       <Navbar />
