f27825143f86b515d656ca3f9f3f0417798b9636
lakshankd <dominiclakshank@gmail.com>
2023-07-08 15:54:00 +0530

    adding new design.

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,10 @@
-import styles from "./page.module.css";
-
-import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
-import Button from "@/components/button/Button";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Navbar />
-      <div className={styles.content}>
-        <h1 className={styles.title}>dominiclakshan</h1>
-        <p className={styles.subTitle}>Full Stack Developer</p>
-        <p className={styles.desc}>
-          Passionate about software engineering, I love creating innovative
-          projects that push boundaries. Explore my portfolio to see my
-          dedication and expertise in action.
-        </p>
-        <Button text="Skills" url="/skills" next={true} />
-      </div>
-      <Footer />
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,28 +1,29 @@
 import React from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
-import Link from "next/link";
 
-export default function Navbar() {
+function Navbar() {
   return (
     <nav className={styles.container}>
-      {/* <div>
-        <Link href="/">
-          <Image src="/arrow.png" width={20} height={20} alt="arrow-btn" />
-        </Link>
-      </div> */}
-      <Link href="/">
-        <button className={styles.btn}>Home</button>
-      </Link>
-      <Link href="/skills">
-        <button className={styles.btn}>Skills</button>
-      </Link>
-      <Link href="/projects">
-        <button className={styles.btn}>Projects</button>
-      </Link>
-      <Link href="/contact">
-        <button className={styles.btn}>Contact</button>
-      </Link>
+      <div className={styles.nav_logo}>
+        <Image
+          src="/logo/logo.png"
+          width={50}
+          height={50}
+          className={styles.nav_logo_img}
+          alt="logo"
+        />
+        <h1>Dominic Lakshan</h1>
+      </div>
+      <div className={styles.nav_links}>
+        <a href="#home">Home</a>
+        <a href="#skills">Skills</a>
+        <a href="#projects">Projects</a>
+        <a href="#interest">Interests</a>
+        <button>Contact</button>
+      </div>
     </nav>
   );
 }
+
+export default Navbar;
