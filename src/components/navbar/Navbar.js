86abec0926c18eebc2cfb01c675a616542a07c9a
lakshankd <dominiclakshank@gmail.com>
2023-07-09 20:33:56 +0530

    adding responsiveness to navbar.

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,12 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
+import { BiMenuAltRight } from "react-icons/bi";
+import { GrClose } from "react-icons/gr";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <nav className={styles.container}>
       <div className={styles.nav_logo}>
@@ -21,6 +25,27 @@ function Navbar() {
         <a href="#projects">Projects</a>
         <a href="#interest">Interests</a>
         <button>Contact</button>
+      </div>
+      <div className={styles.nav_menu}>
+        <div className={styles.nav_menu_icon}>
+          {isMenuOpen ? (
+            <GrClose size={20} onClick={() => setIsMenuOpen((prev) => !prev)} />
+          ) : (
+            <BiMenuAltRight
+              size={25}
+              onClick={() => setIsMenuOpen((prev) => !prev)}
+            />
+          )}
+        </div>
+        {isMenuOpen && (
+          <div className={styles.nav_menu_links}>
+            <a href="#home">Home</a>
+            <a href="#skills">Skills</a>
+            <a href="#projects">Projects</a>
+            <a href="#interest">Interests</a>
+            <button>Contact</button>
+          </div>
+        )}
       </div>
     </nav>
   );
